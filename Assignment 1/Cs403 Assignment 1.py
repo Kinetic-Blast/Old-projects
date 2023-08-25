5009189c482f8824943982a9d277bf153fd72430
Kinetic-Blast <87279302+Kinetic-Blast@users.noreply.github.com>
2023-08-25 17:33:22 -0500

    Add files via upload

---
+++ Assignment 1/Cs403 Assignment 1.py
@@ -0,0 +1,30 @@
+import boto3
+
+#file layout is
+#AWS_ACCESS_KEY_ID
+#AWS_SECRET_ACCESS_KEY
+
+Keys = open ("awskeys.txt","r")
+AWS_ACCESS_KEY_ID =Keys.readline().rstrip()
+AWS_SECRET_ACCESS_KEY =Keys.readline().rstrip()
+
+Name = input("What is your name?: ")
+f = open(Name+".txt", "w")
+f.write(Name)
+f.close()
+
+
+session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+s3 = session.resource('s3')
+
+data = open(Name+".txt", 'rb')
+s3.Bucket('kineticbucket').put_object(Key=Name+".txt", Body=data)
+
+obj = s3.Object('kineticbucket', Name+".txt")
+body = obj.get()['Body'].read()
+
+print(body)
+
+bucket = s3.Bucket('kineticbucket')
+for key in bucket.objects.all():
+    key.delete()
